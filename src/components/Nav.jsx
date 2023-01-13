2b0af14c480090c42cfb91990a2cb361c217ec51
Lee Roberts <lee@Lees-MacBook-Air.local>
2023-01-13 16:10:21 +0000

    This commit will improve UX regarding use of the topics button

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -1,17 +1,19 @@
 import { Link } from 'react-router-dom';
 import { useNavigate } from 'react-router-dom';
 import { useEffect, useState } from 'react';
+import { useParams } from 'react-router-dom';
 import * as api from '../utils/api'
 
 export default function Nav() {
 
     const [menuTopics, setMenuTopics] = useState([]);
+    const [selected, setSelected] = useState("Topics")
 
     useEffect(() => {
         api.fetchTopics().then(({topics}) => {
             setMenuTopics(topics);
         })
-    })
+    }, [selected])
 
 const navigate = useNavigate();
 
@@ -19,11 +21,13 @@ const navigate = useNavigate();
         <span>
         <span><Link className='text_link' to="/"><button className='nav_button'>Home</button></Link> </span>
 
-        <span> <Link className='text_link' to="/all_articles"><button className='nav_button'>View All Articles</button></Link></span>
+        <span> <Link className='text_link' to="/all_articles" onClick={() => {
+            setSelected("Topics")
+        }}><button className='nav_button'>View All Articles</button></Link></span>
 
         <span>
-            <select onChange={(e) => navigate(`${e.target.value}/articles`)} className='nav_button'>
-            <option selected hidden>Topics</option>
+            <select value={selected} onChange={(e) => navigate(`${e.target.value}/articles`)} className='nav_button'>
+            <option selected disabled>Topics</option>
                 {menuTopics.map((topic) => {
                     return <option value={topic.slug} className="text_link">{topic.slug.charAt(0).toUpperCase() + topic.slug.slice(1)}</option>
                 })}
