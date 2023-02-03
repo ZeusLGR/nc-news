5c578dfb928fe5aa79f7dcdd2e07a2c726551085
Lee Roberts <lee@Lees-MacBook-Air.local>
2023-02-03 11:10:34 +0000

    This commit will update the error component to handle 404 errors

--- src/App.js
+++ src/App.js
@@ -1,27 +1,32 @@
-import './App.css';
+import "./App.css";
 import { Routes, Route } from "react-router-dom";
-import Home from './components/Home';
-import Header from './components/Header';
-import Nav from './components/Nav';
-import ArticlesList from './components/ArticlesList';
-import SingleArticle from './components/SingleArticle';
-import { useState } from 'react';
-
+import Home from "./components/Home";
+import Header from "./components/Header";
+import Nav from "./components/Nav";
+import ArticlesList from "./components/ArticlesList";
+import SingleArticle from "./components/SingleArticle";
+import { useState } from "react";
+import ErrorComponent from "./components/ErrorComponent";
 
 function App() {
-  const [selectedSortBy, SetSelectedSortBy] = useState("created_at")
+  const [selectedSortBy, SetSelectedSortBy] = useState("created_at");
   return (
-
     <div className="App">
-      <Header/>
-      <Nav SetSelectedSortBy={SetSelectedSortBy}/>
+      <Header />
+      <Nav SetSelectedSortBy={SetSelectedSortBy} />
       <Routes>
-        <Route path="/" element={<Home/>} />
-        <Route path="/all_articles" element={<ArticlesList selectedSortBy={selectedSortBy} />} />
-        <Route path="/articles/:article_id" element={<SingleArticle/>} />
-        <Route path="/:topic/articles" element={<ArticlesList selectedSortBy={selectedSortBy} />} />
+        <Route path="/" element={<Home />} />
+        <Route path="/*" element={<ErrorComponent />} />
+        <Route
+          path="/all_articles"
+          element={<ArticlesList selectedSortBy={selectedSortBy} />}
+        />
+        <Route path="/articles/:article_id" element={<SingleArticle />} />
+        <Route
+          path="/:topic/articles"
+          element={<ArticlesList selectedSortBy={selectedSortBy} />}
+        />
       </Routes>
-    
     </div>
   );
 }
--- src/components/ErrorComponent.jsx
+++ src/components/ErrorComponent.jsx
@@ -1,16 +1,14 @@
-import { useState } from "react"
+
 
 export default function ErrorComponent({message}) {
 
-const [errorMessage, setErrorMessage] = useState(message);
 
-    if(errorMessage === "Bad request") {
-        setErrorMessage("Sorry, something went wrong with your request. Please refresh the page and try again")
-    }
+
+   
 
 return ( 
     <p className="error">
-        {errorMessage}
+        {message ? "Sorry, something went wrong with your request. Please refresh the page and try again" : "404,not found"}
     </p>
 )
 
